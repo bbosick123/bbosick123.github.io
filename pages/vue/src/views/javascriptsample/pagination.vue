dafed603e7997e512c214b60d98c4697d4ec24e6
bbosick123 <bbosick123@ebsap.co.kr>
2024-08-05 15:05:30 +0900

    ㅁ
    ㅁ

--- pages/vue/src/views/javascriptsample/pagination.vue
+++ pages/vue/src/views/javascriptsample/pagination.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<h4 class="contents__title">Pagination</h4>
-		<ContentItem date="2024년 08월 05일"> 1 </ContentItem>
+		<ContentItem date="2024년 08월 05일"> 링크 - <router-link to="../sample/Pagination">Pagination</router-link> </ContentItem>
 	</div>
 </template>
 
